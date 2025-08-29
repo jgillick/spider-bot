2d988f85fe72bef6051a708abf2935e274fbb9fa
Jeremy <j_gillick@yahoo.com>
2025-08-28 23:42:51 -0700

    Let the range change on command managers

--- simulate/genesis/genesis_forge/managers/command/command_manager.py
+++ simulate/genesis/genesis_forge/managers/command/command_manager.py
@@ -7,7 +7,8 @@ import genesis as gs
 from genesis_forge.genesis_env import GenesisEnv
 from genesis_forge.managers.base import BaseManager
 
-CommandRange = Tuple[float, float]
+CommandRangeValue = Tuple[float, float]
+CommandRange = Union[CommandRangeValue, dict[str, CommandRangeValue]]
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -54,26 +55,68 @@ class CommandManager(BaseManager):
 
     Args:
         env: The environment to control
-        range: The number range, or list of ranges, to generate target values for
+        range: The number range, or dict of ranges, to generate target command(s) for
         resample_time_s: The time interval between changing the command
     """
 
     def __init__(
         self,
         env: GenesisEnv,
-        range: Union[CommandRange, dict[str, CommandRange]],
-        resample_time_s: float = 5.0,
+        range: CommandRange,
+        resample_time_sec: float = 5.0,
     ):
         super().__init__(env)
-        self.range = range
+        self._range = range
+        self.resample_time_sec = resample_time_sec
+
         num_ranges = len(range) if isinstance(range, dict) else 1
         self._command = torch.zeros(env.num_envs, num_ranges, device=gs.device)
-        self._resample_steps = int(resample_time_s / env.dt)
+
+    """
+    Properties
+    """
 
     @property
     def command(self) -> torch.Tensor:
-        """The desired base velocity command in the base frame. Shape is (num_envs, 3)."""
+        """The desired base velocity command in the base frame. Shape is (num_envs, num_ranges)."""
         return self._command
+    
+    @property
+    def range(self) -> CommandRange:
+        """The range of values to generate target command(s) for."""
+        return self._range
+    
+    @range.setter
+    def range(self, range: CommandRange):
+        """Set the range of values to generate target command(s) for."""
+        # Validate the shape of the range
+        num = len(range) if isinstance(range, dict) else 1
+        if num != self._command.shape[1]:
+            raise ValueError(f"Cannot change the shape of the CommandManager range. Expected size: {self._command.shape[1]}, got {num}")
+        # Validate the range types match
+        if type(range) != type(self._range):
+            raise ValueError(f"Cannot change the base type of the CommandManager range. Expected type: {type(self._range)}, got {type(range)}")
+        # Validate that the dict keys match the current range dict keys
+        if isinstance(range, dict):
+            if set(range.keys()) != set(self._range.keys()):
+                raise ValueError(f"Cannot change the dict keys of the CommandManager range. Expected keys: {set(self._range.keys())}, got {set(range.keys())}")
+        self._range = range
+    
+    @property
+    def resample_time_sec(self) -> float:
+        """The time interval (in seconds) between changing the command for each environment."""
+        return self._resample_time_sec
+    
+    @resample_time_sec.setter
+    def resample_time_sec(self, resample_time_s: float):
+        """Set the time interval (in seconds) between changing the command for each environment."""
+        self._resample_time_sec = resample_time_s
+        self._resample_steps = int(resample_time_s / self.env.dt)
+    
+
+    """
+    Operations
+    """
 
     def step(self):
         """Resample the command if necessary"""
@@ -95,16 +138,20 @@ class CommandManager(BaseManager):
             env_ids = torch.arange(self.env.num_envs, device=gs.device)
         self._resample_command(env_ids)
 
+    """
+    Implementation
+    """
+
     def _resample_command(self, env_ids: Sequence[int]):
         """Create a new command for the given environment ids."""
         num = torch.empty(len(env_ids), device=gs.device)
 
         # Get range values (this might have changed since init due to curriculum training)
         ranges = None
-        if isinstance(self.range, dict):
-            ranges = list(self.range.values())
+        if isinstance(self._range, dict):
+            ranges = list(self._range.values())
         else:
-            ranges = [self.range]
+            ranges = [self._range]
 
         # Resample the command
         for i in range(self._command.shape[1]):
