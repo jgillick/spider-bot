fd55009460e8d958de31e1acaf5ad466d69e4fdf
Jeremy Gillick <j_gillick@yahoo.com>
2025-09-01 15:47:34 -0700

    Add terrain manager

--- simulate/genesis/genesis_forge/managers/__init__.py
+++ simulate/genesis/genesis_forge/managers/__init__.py
@@ -1,14 +1,18 @@
+from .base import BaseManager
 from .reward_manager import RewardManager
 from .termination_manager import TerminationManager
 from .action.positional_action_manager import PositionalActionManager
 from .command import CommandManager, VelocityCommandManager
 from .contact_manager import ContactManager
+from .terrain_manager import TerrainManager
 
 __all__ = [
+    "BaseManager",
     "RewardManager",
     "TerminationManager",
     "CommandManager",
     "VelocityCommandManager",
     "PositionalActionManager",
     "ContactManager",
+    "TerrainManager",
 ]
